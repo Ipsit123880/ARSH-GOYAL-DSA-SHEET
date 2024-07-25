041e028585b3947b4351db2b5fa2d91ef2db4392
Ipsit123880 <108969878+Ipsit123880@users.noreply.github.com>
2024-07-25 08:17:26 +0530

    Add files via upload

---
+++ 2. Arrays_sort_Colors.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                zeros++;
+            }
+            else if(nums[i] == 1){
+                ones++;
+            }
+            else{
+                twos++;
+            }
+        }
+
+
+        for(int i = 0; i < n; i++){
+            if( i < zeros){
+                nums[i] = 0;
+            }
+            else if(i < zeros + ones){
+                nums[i] = 1;
+            }
+            else{
+                nums[i] = 2;
+            }
+        }
+
+        
+    }
+};
